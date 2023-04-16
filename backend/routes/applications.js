1117f0ea8c59e74226642c806183dead87e348f8
Mohammed-Alsaad <mohammad.sadiq.alsaad@gmail.com>
2023-04-15 23:54:00 -0500

    backend update

--- backend/routes/applications.js
+++ backend/routes/applications.js
@@ -3,4 +3,18 @@ const router = express.Router()
 const Application = require('../models/application')
 const User = require('../models/user')
 
+// GET all
+router.get('/', async (req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.query.userid })
+        const applications = []
+        for (let i = 0; i < user.applications.length; i++) {
+            applications.push(await Application.findById(user.applications[i]))
+        }
+        return res.status(200).json({ message: "OK", data: applications })
+    } catch (err) {
+        return res.status(500).json({ message: err.message, data: [] })
+    }
+})
+
 module.exports = router
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -40,4 +40,28 @@ router.post('/', async (req, res) => {
     }
 })
 
+// Middleware for getting specific user
+async function getUser(req, res, next) {
+    let user
+    try {
+        user = await User.findById(req.params.id)
+        if (user == null) {
+            return res.status(404).json({ message: "Cannot find user", data: {} })
+        }
+    } catch (err) {
+        return res.status(404).json({ message: "ID is not valid", data: {} })
+    }
+    res.user = user
+    next()
+}
+
+// GET name
+router.get('/:id', getUser, async (req, res) => {
+    try {
+        return res.status(200).json({ message: "OK", data: res.user })
+    } catch (err) {
+        return res.status(500).json({ message: err.message, data: [] })
+    }
+})
+
 module.exports = router
